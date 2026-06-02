65ad2bd08b0d597d886fa3fe0f97ea97049c0b73
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-02 17:39:11 -0500

    feat: implementar enrutamiento con react-router-dom y agregar página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/main-page';
+
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <Routes>
+      <Route
+        path='/'
+        element={<MainPage />}
+      />
+    </Routes>
   );
 }
 
