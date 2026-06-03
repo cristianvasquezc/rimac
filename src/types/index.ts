462333ca868d7e4f4fe0f05509b6b0e33d1352ef
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-03 18:47:36 -0500

    feat: agregar interfaces de tipos para User, Plan y AppState en index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,46 @@
+export interface User {
+  name: string;
+  lastName: string;
+  birthDay: string;
+}
+
+export interface Plan {
+  name: string;
+  price: number;
+  description: string[];
+  age: number;
+}
+
+export interface PlansResponse {
+  list: Plan[];
+}
+
+export interface QuoteFormData {
+  dni: string;
+  phone: string;
+  privacyPolicy: boolean;
+  marketingConsent?: boolean;
+}
+
+export type UserType = 'for-me' | 'for-someone-else';
+
+export interface AppState {
+  quoteFormData: QuoteFormData | null;
+  setQuoteFormData: (data: QuoteFormData) => void;
+
+  user: User | null;
+  setUser: (user: User) => void;
+
+  userType: UserType | null;
+  setUserType: (type: UserType) => void;
+
+  plans: Plan[];
+  setPlans: (plans: Plan[]) => void;
+
+  selectedPlan: Plan | null;
+  setSelectedPlan: (plan: Plan) => void;
+
+  calculateUserAge: () => number | null;
+
+  reset: () => void;
+}
