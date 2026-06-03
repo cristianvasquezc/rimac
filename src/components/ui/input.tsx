09b3bd4c29c7872521291fb64507a2d091ebbfdf
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-03 11:50:31 -0500

    feat: agregar clase de enfoque y eliminar clase de contorno en el componente Input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       type={type}
       data-slot='input'
       className={cn(
-        'h-12 w-full min-w-0 rounded-md border border-border bg-transparent px-2.5 py-2.5 text-base font-normal leading-7 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-foreground/60 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-red-500 md:text-sm',
+        'h-12 w-full min-w-0 rounded-md border border-border bg-transparent px-2.5 py-2.5 text-base font-normal leading-7 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-foreground/60 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50  focus:border-blue-500 aria-invalid:border-red-500 md:text-sm outline-none',
         className,
       )}
       {...props}
