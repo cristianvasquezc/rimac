8ad1dc06e9eb8a74496d803d3f3e327214220962
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-03 09:55:42 -0500

    feat: implementar componente Navbar con enlaces y diseño responsivo

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,3 +1,25 @@
+import { PhoneIcon } from '@heroicons/react/24/solid';
+import { Logo } from '../Icons';
+
 export const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <nav className='w-full  bg-white px-5'>
+      <div className='w-full max-w-6xl flex items-center justify-between py-5 mx-auto'>
+        <a href='/'>
+          <Logo />
+        </a>
+        <div className='flex items-center space-x-4'>
+          <p className='font-semibold text-xs tracking-wide hidden md:block'>
+            ¡Compra por este medio!
+          </p>
+          <a
+            href='tel:014116001'
+            className='flex items-center justify-center gap-2 font-bold text-lg leading-5 tracking-wide'>
+            <PhoneIcon className='size-5' />
+            (01) 411 6001
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 };
