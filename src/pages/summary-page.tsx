2ae156e932b09aa2e8904de51e119368945accdb
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-03 17:25:00 -0500

    feat: implementar la página de resumen con componentes Breadcrumb y CardSummary

--- src/pages/summary-page.tsx
+++ src/pages/summary-page.tsx
@@ -1,5 +1,23 @@
+import { CardSummary } from '@/components/cards/card-summary';
+import { Breadcrumb } from '@/components/ui/breadcrumb';
+
 function SummaryPage() {
-  return <div>Summary Page</div>;
+  return (
+    <div className='font-lato bg-white pb-10'>
+      <Breadcrumb
+        currentStep={2}
+        href='/planes'
+      />
+
+      <div className='w-full max-w-4xl space-y-5 px-6 mx-auto'>
+        <h1 className='font-bold text-[28px] md:text-[40px] leading-10 md:leading-12 tracking-[-0.6px]'>
+          Resumen del seguro
+        </h1>
+
+        <CardSummary />
+      </div>
+    </div>
+  );
 }
 
 export default SummaryPage;
