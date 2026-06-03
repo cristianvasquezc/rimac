6f179762b6a676c9df800ef024fd6dec78c007ac
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-02 21:12:59 -0500

    feat: agregar componente Navbar y actualizar MainLayout para incluirlo

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { Navbar } from '../components/navbar/navbar';
 
 function MainLayout() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
