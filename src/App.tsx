a68b071c39588844cd0441d3c6536874da3458f8
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-02 21:12:15 -0500

    feat: actualizar componente App para incluir MainLayout y rutas anidadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
+import MainLayout from './layouts/main-layout';
 import MainPage from './pages/main-page';
+import Planes from './pages/plans-page';
+import Resumen from './pages/summary-page';
 
 function App() {
   return (
     <Routes>
       <Route
         path='/'
-        element={<MainPage />}
-      />
+        element={<MainLayout />}>
+        <Route
+          index
+          element={<MainPage />}
+        />
+        <Route
+          path='/planes'
+          element={<Planes />}
+        />
+        <Route
+          path='/resumen'
+          element={<Resumen />}
+        />
+      </Route>
     </Routes>
   );
 }
