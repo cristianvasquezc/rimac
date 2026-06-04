112658adcb2d1c56628e29b5fc9a5d5eb9da9782
cristianvasquezc <cristianvasquez1312@gmail.com>
2026-06-03 19:21:32 -0500

    feat: reemplazar etiquetas <a> por componentes <Link> en Navbar y Breadcrumb para mejorar la navegación

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,23 +1,24 @@
 import { PhoneIcon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 import { Logo } from '../Icons';
 
 export const Navbar = () => {
   return (
     <nav className='w-full  bg-white px-6'>
       <div className='w-full max-w-6xl flex items-center justify-between py-5 mx-auto'>
-        <a href='/'>
+        <Link to='/'>
           <Logo />
-        </a>
+        </Link>
         <div className='flex items-center space-x-4'>
           <p className='font-semibold text-xs tracking-[0.2px] hidden md:block'>
             ¡Compra por este medio!
           </p>
-          <a
-            href='tel:014116001'
+          <Link
+            to='tel:014116001'
             className='flex items-center justify-center gap-2 font-bold text-lg leading-5 tracking-[0.4px]'>
             <PhoneIcon className='size-5' />
             (01) 411 6001
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { ChevronLeftIcon } from '@heroicons/react/16/solid';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface BreadcrumbProps {
   currentStep: 1 | 2;
@@ -70,8 +71,8 @@ export const Breadcrumb = ({ currentStep, href, hidden }: BreadcrumbProps) => {
       </div>
       <div className='w-full max-w-4xl p-4 md:py-10 px-6 mx-auto border-b md:border-none border-border mb-8 md:mb-0'>
         <div className='flex items-center gap-4'>
-          <a
-            href={href}
+          <Link
+            to={href}
             className='w-fit flex item-center justify-start gap-2'>
             <span className='w-5 h-5 flex items-center justify-center rounded-full text-sm border-2 border-[#A9AFD9] md:border-[#4F4FFF]'>
               <ChevronLeftIcon className='size-4 text-[#4F4FFF]' />
@@ -79,7 +80,7 @@ export const Breadcrumb = ({ currentStep, href, hidden }: BreadcrumbProps) => {
             <p className='font-bold text-lg leading-5 tracking-[0.4px] hidden md:block text-[#4F4FFF]'>
               Volver
             </p>
-          </a>
+          </Link>
           <p className='font-black text-xs leading-4 tracking-[0.8px] uppercase block md:hidden'>
             Paso 1 de 2
           </p>
